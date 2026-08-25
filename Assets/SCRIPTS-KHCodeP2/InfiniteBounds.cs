078c7f04e7213cc24c45eeb2480b0f5958ddfc56
Daniel Dentone <143889531+Bythalassa@users.noreply.github.com>
2026-08-25 10:22:42 -0500

    KHCodeP2 - InfiniteBounds.cs

---
+++ Assets/SCRIPTS-KHCodeP2/InfiniteBounds.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+
+
+/*-> Script Objective
+SRC Game Idea: KHCodeP2 : https://www.youtube.com/watch?v=TQyjAWp_CyY 
+
+Requerieientos: 
+
+The physicial objects in game: The Player and The Asteroids.
+Asteroids: MOVE the object to the other side of the Screen 
+IF they EXIT BOUNDARIES on the Viewpoint.
+
+is like a reuse object from screen, kinda thing. 
+*/
+
+
+public class InfiniteBounds : MonoBehaviour
+{
+
+
+
+
+
+}
