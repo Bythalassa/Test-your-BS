1464440e0e6da05941658d8a6a502a53fc8acebc
Bythalassa <143889531+Bythalassa@users.noreply.github.com>
2026-08-25 14:20:09 -0500

    ClassInstanceTC2W23.cs done x2<-

--- Assets/SCRIPTS-TecC2Week2-3/ClassInstanceTC2W23.cs
+++ Assets/SCRIPTS-TecC2Week2-3/ClassInstanceTC2W23.cs
@@ -1,16 +1,37 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ClassInstanceTC2W23 : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Enemy skull = new Enemy();
+        skull.EnemyName = "Skull";
+        skull.Hp = 8;
+
+        Enemy witch = new Enemy();
+        witch.EnemyName = "Witch";
+        witch.Hp = 50;
     }
 
-    // Update is called once per frame
-    void Update()
+    public class Enemy
     {
+        public string EnemyName;
+        public float Hp;
         
-    }
+        public void TakeDamage(float damage)
+        {
+            Hp -= damage;
+        }
+
+        public void Death()
+        {
+            if (Hp >= 0f)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+
+
 }
